b182af8688b392c2d5b513cd9051c66a3152c6ea
tapia1013 <manbearpig1013@gmail.com>
2021-11-13 20:12:34 -0800

    fixed where i forgot to add .js extension in backend ordercontroller for orderRoutes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router()
 import {
   addOrderItems
-} from '../controllers/orderController';
+} from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 
